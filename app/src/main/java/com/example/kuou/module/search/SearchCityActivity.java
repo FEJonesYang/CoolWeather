e07cfcde37ba7017801c4926c30199bb8f60a468
Yangchao <545661033@qq.com>
2021-04-15 12:30:44 +0800

    feature:实体类以及城市搜索UI

---
+++ app/src/main/java/com/example/kuou/module/search/SearchCityActivity.java
@@ -0,0 +1,16 @@
+package com.example.kuou.module.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kuou.R;
+
+public class SearchCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_city);
+    }
+}

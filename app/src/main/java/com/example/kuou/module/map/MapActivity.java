862ae3ec7dc93e744bbf6906cf1990ebb735026d
yangchao <545661033@qq.com>
2021-06-12 15:22:22 +0800

    fit:获取地图的定位，点击时获取位置信息

--- app/src/main/java/com/example/kuou/module/map/MapActivity.java
+++ app/src/main/java/com/example/kuou/module/map/MapActivity.java
@@ -2,16 +2,27 @@ package com.example.kuou.module.map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.example.kuou.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements AMap.OnMyLocationChangeListener, AMap.OnMapClickListener {
+
+    private static final String TAG = "MapActivity";
+    MapView mMapView = null;
+    AMap aMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +38,80 @@ public class MapActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+
+        // 展示地图蓝色的点
+        showBluePoint();
+
+        // 设置手势点击，获取点击的位置信息
+        aMap.setOnMapClickListener(this);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 展示地图蓝点
+     */
+    private void showBluePoint() {
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//定位一次，且将视角移动到地图中心点。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false
+    }
+
+    /**
+     * 获得定位监听的位置
+     *
+     * @param location
+     */
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onMyLocationChange(Location location) {
+        Log.d(TAG + "init listener -- >", String.valueOf(location.getLatitude()) + "," + String.valueOf(location.getLongitude()));
+    }
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d(TAG + "click listener -- >", String.valueOf(latLng.longitude) + "," + String.valueOf(latLng.latitude));
     }
 }
